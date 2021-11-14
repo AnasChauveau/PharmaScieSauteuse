75fa7b66c93218d8c0e9170bbc259f4f0c5b3858
Anas CHAUVEAU <anas38.gt@gmail.com>
2021-11-14 16:31:22 +0100

    Ajout des requetes SELECT de la page gestion des patients et fomatage de la date de naissance

--- Backend/controllers/pharmaController.js
+++ Backend/controllers/pharmaController.js
@@ -1,13 +1,16 @@
+const mysqlconnexion = require('../models/connexion')
+const moment = require ("../config/moment")
+
 const pharMenu = (req, res) => {
     res.render('menu')
 }
 
 const pharmAffichagePatients = (req, res) => {
-    let requete = "SELECT CONCAT(Nom_Patient, ' ', Prenom_Patient) AS Nom_Patient FROM patient"
+    let requete = "SELECT CONCAT(Nom_Patient, ' ', Prenom_Patient) AS Nom_Patient, noSS, Date_naissance FROM patient order by Nom_Patient, Prenom_Patient"
     mysqlconnexion.query(requete, (err, lignes, champs) => {
         if (!err) {
             console.log(lignes)
-            res.render('patient', {patients : lignes})
+            res.render('patient', {patients : lignes, moment : moment})
         }else{
             res.redirect('erreur')
         }
--- Backend/index.js
+++ Backend/index.js
@@ -6,16 +6,6 @@ const iniparser = require('iniparser')
 
 const Routeur = require('./routes/pharmaRoute')
 
-
-// récupération des paramètres et préparation de connexion à la BDD
-let configDB = iniparser.parseSync('./DB.ini')
-let mysqlconnexion = mysql.createConnection({
-    host:configDB['dev']['host'],
-    user:configDB['dev']['user'],
-    password:configDB['dev']['password'],
-    database:configDB['dev']['database']
-})
-
 // activation des dépendances 
 let app = express()
 app.set('view engine', 'ejs')
@@ -24,11 +14,6 @@ app.use(express.static('public'))
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-// connexion à la base de données
-mysqlconnexion.connect((err) => {
-    if (!err) console.log('BDD connectée.')
-    else console.log('BDD connexion échouée \n Erreur: '+JSON.stringify(err))
-})
 
 app.listen(3000, () => console.log('le serveur est prêt.'))
 
